4a2f1005a97d5431ffcab14c1c46c35aa535216e
Hemant Bhabal <b.hemant1994@gmail.com>
2021-12-31 01:19:05 +0530

    keypad close story

--- lib/screens/Auth/verification.dart
+++ lib/screens/Auth/verification.dart
@@ -427,6 +427,9 @@ class _VerificationScreenState extends State<VerificationScreen> {
                 } else {
                   FocusScope.of(context).requestFocus(previousFocusNode);
                 }
+                if(validate()){
+                  FocusScope.of(context).requestFocus(FocusNode());
+                }
               },
         keyboardType: TextInputType.phone,
         maxLength: 1,
